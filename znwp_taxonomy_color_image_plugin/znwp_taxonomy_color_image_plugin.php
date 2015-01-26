415f8743e5013dcbe033c41e5623eaf6d34d0c4e
Ng Kitt Horng, Zion <zion@intzone.com>
2015-01-27 00:57:33 +0800

    Create znwp_taxonomy_color_image_plugin.php

---
+++ znwp_taxonomy_color_image_plugin/znwp_taxonomy_color_image_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: ZnWP Taxonomy Color and Image Plugin
+Plugin URI:  https://github.com/zionsg/ZnWP-Taxonomy-Color-Image-Plugin
+Description: This plugin allows multiple 3rd party plugins to easily add color and image fields to taxonomies. Demo plugin included.
+Author:      Zion Ng
+Author URI:  http://intzone.com/
+Version:     1.0.0
+*/
+
+require_once 'ZnWP_Taxonomy_Color_Image.php'; // PSR-1 states files should not declare classes AND execute code
+
+// init must be run after theme setup to allow functions.php in theme to add action hook
+$znwp_taxonomy_color_image_plugin = new ZnWP_Taxonomy_Color_Image();
+add_action('after_setup_theme', array($znwp_taxonomy_color_image_plugin, 'init'));
